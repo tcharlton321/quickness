238a1b65ee348cf637eb2c456af714f687c8e18b
tcharlton321 <timothycharlton321@gmail.com>
2021-05-05 16:04:28 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [qbtn, setButton] = useState({
+    score: 0,
+    x_coord: 50,
+    y_coord: 50,
+  });
+
+  const [xpos, setXpos] = useState(qbtn.x_coord);
+  const [ypos, setYpos] = useState(qbtn.y_coord);
+  const [trans, setTrans] = useState(0);
+
+  const modifyScore = (n: number) => {
+    const newBtn = ({
+      score: qbtn.score + n,
+      x_coord: qbtn.x_coord,
+      y_coord: qbtn.y_coord, });
+      setButton(newBtn);
+  }
+
+  const modifyCoordinates = () => {
+    setXpos(Math.random() * 95);
+    setYpos(Math.random() * 95);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+          style={{left: xpos+'%', top: ypos+'%'}}
+          type="button"
+          className="qbtn"
+          onClick={e => {
+              e.preventDefault();
+              modifyScore(1);
+          }}
+          onMouseEnter={e => {
+              e.preventDefault();
+              modifyCoordinates();
+              }}
+              
+          >
+          {qbtn.score}
+      </button>
     </div>
   );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+interface QBTN {
+    score: number;
+    x_coord: number;
+    y_coord: number;
+}
+
+type ModifyScore = (n: number) => void;
+
+
+
